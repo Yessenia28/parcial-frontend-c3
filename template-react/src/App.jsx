dcc66aadeec9bf03f80c179784f2e1d45ed950a5
Yessenia28 <yesebr24@mail.com>
2024-11-04 22:21:28 -0500

    Proyecto parcial

---
+++ template-react/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Carga de estudiantes</h1>
+      <form ></form>
+      <Card />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ template-react/src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = () => {
+  return 
+  
+    <div>Hola, Soy uns tsrjeta</div>;
+};
+
+export default Card;
